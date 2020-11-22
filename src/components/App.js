d05a38c2158baea6e9afaad25f44d1b006d37c4b
DK <i386.net@gmail.com>
2020-11-22 09:35:36 +0300

    add useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter -1 );
+
   return (
     <div className="App">
-
+      <button className="btn" onClick={increment}>Нажми меня!</button>
+      <button className="btn" onClick={decrement}>Нажми меня!</button>
+      <p>Пользователи: {counter}</p>
     </div>
   );
 }
