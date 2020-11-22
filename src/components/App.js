a8a36f0618581489c9bce6a9ec7e2ba8753e89c9
DK <i386.net@gmail.com>
2020-11-22 15:26:54 +0300

    add fetching data and clear effect in useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [data, setData] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [type, setType] = useState('users');
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  })
 
   const increment = () => setCounter(counter + 1);
   const decrement = () => setCounter(counter -1 );
+  const visibilityHandler = () => setVisible(!visible);
+  const mouseMoveHandler = (e) => setPosition({
+    x: e.clientX,
+    y: e.clientY,
+  });
+
+  useEffect(() => {
+    console.log('Rendering  data');
+    if (visible) {
+      fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(response => response.json())
+        .then(json => setData(json))
+    }
+  }, [visible, type]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMoveHandler);
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+    }
+  }, [position]);
+
+  const dataList = data.slice(0, 10).map(d => <li key={d.id}>{JSON.stringify(d)}</li>)
 
   return (
     <div className="App">
@@ -14,6 +45,17 @@ function App() {
         <button className="btn" onClick={decrement}>Down</button>
       </div>
       <p>Пользователи: {counter}</p>
+      <hr className="line"/>
+      <p>x: {position.x} y: {position.y}</p>
+      <hr className="line"/>
+      <button className="btn" onClick={visibilityHandler}>Show Data</button>
+      <button className="btn" onClick={() => setType('users')}>Users</button>
+      <button className="btn" onClick={() => setType('posts')}>Posts</button>
+      <button className="btn" onClick={() => setType('todos')}>Todos</button>
+      <p>Current type: {type}</p>
+      <ul className={visible? 'on': 'off'}>
+        {dataList}
+      </ul>
     </div>
   );
 }
